81178a01dc46b2a78160410c64c176e53953b24e
Norman Koch <norman.koch@tu-dresden.de>
2021-01-30 16:17:12 +0100

    added reddit google search

--- index.php
+++ index.php
@@ -12,6 +12,9 @@
                 $param = preg_replace("/\s*!dwiki/", "", $param);
                 $url = "https://de.wikipedia.org/w/index.php?search=".urlencode($param)."&title=Spezial%3ASuche&fulltext=Suchen&ns0=1";
 
+        } else if(preg_match('/!gr/', $param)) {
+                $param = preg_replace("/\s*!gr/", "", $param);
+                $url = "index.php?param=".urlencode("!g site:reddit.com $param");
         }
         header('Location: '.$url);
         exit;
